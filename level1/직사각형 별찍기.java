0845ee6ceb96d0f9b79f7aefe16ac4e7594a04c4
Sangyun Kim <74143534+SangYun0922@users.noreply.github.com>
2022-06-30 22:32:30 +0900

    20220630solved

--- level1/직사각형 별찍기.java
+++ level1/직사각형 별찍기.java
@@ -1,15 +1,14 @@
-public class 음양더하기 {
-    public int solution(int[] absolutes, boolean[] signs) {
-        int answer = 0;
-        for (int i = 0; i < absolutes.length; i++)
-            answer += absolutes[i]*(signs[i] ?1:-1);
-        return answer;
-    }
-
-    public static void main(String[] args){
-        int absolutes[] = {4,7,12};
-        boolean signs[] = {true, false, true};
-        음양더하기 answer = new 음양더하기();
-        System.out.println("solution(absolutes, signs) = " + answer.solution(absolutes, signs));
+import java.util.Scanner;
+public class 직사각형별찍기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
