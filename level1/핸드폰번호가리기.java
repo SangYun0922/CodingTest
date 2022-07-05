0389afb6729de3a9171877a0a68342427fe4b3d6
Sangyun Kim <74143534+SangYun0922@users.noreply.github.com>
2022-07-06 08:54:36 +0900

    repeat update

--- level1/핸드폰번호가리기.java
+++ level1/핸드폰번호가리기.java
@@ -1,10 +1,13 @@
 public class 핸드폰번호가리기 {
     public String solution(String phone_number) {
             String answer = "";
-            String encrypt = phone_number.substring(phone_number.length()-4, phone_number.length());
-            for (int i = 0; i < phone_number.length()-4 ; i++)
-                answer += "*";
-            answer += encrypt;
+            String encrypt = "*";
+            String num = phone_number.substring(phone_number.length()-4, phone_number.length());
+            answer += encrypt.repeat(phone_number.length()-4);
+            // for (int i = 0; i < phone_number.length()-4 ; i++)
+            //     answer += "*";
+            answer += num;
+            System.out.println(answer);
             return answer;
     }
     public static void main(String[] args) {
