1ba17f020454612cc0942fdf2c70e8a2027ef6d1
Sangyun Kim <74143534+SangYun0922@users.noreply.github.com>
2022-07-21 21:40:45 +0900

    20220721solved

---
+++ level1/수박수박수박수박수박수.java
@@ -0,0 +1,16 @@
+public class 수박수박수박수박수박수 {
+    public String solution(int n) {
+        String answer = "";
+        for (int i = 1; i <= n; i++)
+            if (i % 2 == 1)
+                answer += "수";
+            else
+                answer += "박";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        수박수박수박수박수박수 answer = new 수박수박수박수박수박수();
+        System.out.println("answer.solution(3) = " + answer.solution(3));
+    }
+}
