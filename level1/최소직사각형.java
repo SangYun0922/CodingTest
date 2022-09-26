d389e0b80c4dc2a46a89815ea28b3dea90ecec38
Sangyun Kim <74143534+SangYun0922@users.noreply.github.com>
2022-09-26 23:43:40 +0900

    20220926solved

---
+++ level1/최소직사각형.java
@@ -0,0 +1,19 @@
+public class 음양더하기 {
+    public int solution(int[][] sizes) {
+        int max = 0;
+        int min = 0;
+        for (int[] size : sizes) {
+            int paramMax = Math.max(size[0], size[1]);
+            int paramMin = Math.min(size[0], size[1]);
+
+            if (paramMax > max) {
+                max = paramMax;
+            }
+
+            if (paramMin > min) {
+                min = paramMin;
+            }
+        }
+        return min * max;
+    }
+}
