c72b68365633b861dd62bacc422659bfcd55f94e
Sangyun Kim <74143534+SangYun0922@users.noreply.github.com>
2022-09-28 16:35:20 +0900

    20220928solved

--- level1/두정수사이의합.java
+++ level1/두정수사이의합.java
@@ -1,20 +1,35 @@
+package programmers.level1;
+
 public class 두정수사이의합 {
-    public long solution(int a, int b) {
+    public long solution1(int a, int b) {
         long answer = 0;
-        if(a<=b){
-            for(int i= a ; i<=b; i++){
-                answer += i;
-            }
-        } else {
-            for(int i= b; i<=a; i++){
-                answer += i;
+        int big, small;
+        if (a == (b * -1)) // 두 수의 부호가 다를때
+            return 0;
+        else {
+            if (a > b) {
+                big = a;
+                small = b;
+            } else {
+                big = b;
+                small = a;
             }
         }
+        for (int i = small ; i <= big ; i++)
+            answer += i;
         return answer;
     }
 
+    public long solution2(int a, int b) {
+        long answer = 0;
+        long small = Math.min(a,b);
+        long big = Math.max(b,a);
+        answer = (big - small + 1) * (small + big) / 2;
+        return answer;
+    }
     public static void main(String[] args) {
         두정수사이의합 answer = new 두정수사이의합();
-        System.out.println("answer.solution(3,5) = " + answer.solution(3,5));
+//        System.out.println("answer.solution1(-10000000,10000000) = " + answer.solution1(-10000000,10000000));
+        System.out.println("answer.solution2(-10000000,10000000) = " + answer.solution2(-10000000,10000000));
     }
 }
