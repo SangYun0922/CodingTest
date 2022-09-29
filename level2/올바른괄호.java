929837b2fe0ca6e0a2da53077c4779e884c2a319
Sangyun Kim <74143534+SangYun0922@users.noreply.github.com>
2022-09-29 12:29:09 +0900

    20220929solved

--- level2/올바른괄호.java
+++ level2/올바른괄호.java
@@ -1,32 +1,40 @@
+package programmers.level2;
 import java.util.*;
-
 public class 올바른괄호 {
-    public boolean solution(String s) {
-        Deque<Character> deque = new ArrayDeque<>();
+    boolean solution(String s) { // 큐 안쓴 풀이
+        int answer = 0;
+        if (s.charAt(0) == ')')
+            return false;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                deque.push('(');
-            } else if (s.charAt(i) == ')') {
-                if (deque.isEmpty()) {
+            if (s.charAt(i) == '(')
+                answer++;
+            else if (answer > 0)
+                answer--;
+            else
+                return false;
+        }
+        return (answer == 0);
+    }
+
+    boolean solution_queue(String s) { //큐 쓴 풀이
+        Deque<String> deque = new ArrayDeque<>();
+        if (s.charAt(0) == ')')
+            return false;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(')
+                deque.push("(");
+            else {
+                if (deque.isEmpty())
                     return false;
-                } else {
+                else {
                     deque.pop();
                 }
             }
         }
-        return (deque.isEmpty()) ? true : false;
+        return deque.isEmpty();
     }
-
     public static void main(String[] args) {
-
         올바른괄호 answer = new 올바른괄호();
-        String s1 = "()()";
-        String s2 = "(())()";
-        String s3 = ")()(";
-        String s4 = "(()(";
-        System.out.println("answer.solution() = " + answer.solution(s1));
-        System.out.println("answer.solution() = " + answer.solution(s2));
-        System.out.println("answer.solution() = " + answer.solution(s3));
-        System.out.println("answer.solution() = " + answer.solution(s4));
+        System.out.println("answer.solution = " + answer.solution("(()("));
     }
 }
