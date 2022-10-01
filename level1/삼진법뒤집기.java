ee422f6d7faad32ac61c07eacbcc288dfa587a70
Sangyun Kim <74143534+SangYun0922@users.noreply.github.com>
2022-10-01 13:10:24 +0900

    20221001solved

--- level1/삼진법뒤집기.java
+++ level1/삼진법뒤집기.java
@@ -1,7 +1,7 @@
 package programmers.level1;
 
 public class 삼진법뒤집기 {
-    public int solution(int n) {
+    public int solution1(int n) { //기존 풀이
         int answer = 0;
         StringBuffer temp = new StringBuffer();
         while (n != 0) {
@@ -9,14 +9,25 @@ public class 삼진법뒤집기 {
             n /= 3;
         }
         String reverse = temp.toString();
+        System.out.println("reverse = " + reverse);
         for (int i = 0; i < reverse.length(); i++) {
             answer += Character.getNumericValue(reverse.charAt(reverse.length()-1-i)) * Math.pow(3, i);
         }
         return answer;
     }
 
+    public int solution2(int n) { //최적화 풀이
+        StringBuffer temp = new StringBuffer();
+        while (n > 0) {
+            temp.append(n%3);
+            n /= 3;
+        }
+        return Integer.parseInt(temp.toString(), 3);
+    }
+
     public static void main(String[] args) {
         삼진법뒤집기 answer = new 삼진법뒤집기();
-        System.out.println("answer. = " + answer.solution(45));
+        System.out.println("answer. = " + answer.solution1(45));
+        System.out.println("answer. = " + answer.solution2(45));
     }
 }
