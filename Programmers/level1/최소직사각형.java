2bc680f28790b9966caa121b16825ba95c56d892
Sangyun Kim <74143534+SangYun0922@users.noreply.github.com>
2022-10-10 23:09:56 +0900

    20221010solved

--- Programmers/level1/최소직사각형.java
+++ Programmers/level1/최소직사각형.java
@@ -1,19 +1,21 @@
-public class 음양더하기 {
+public class 최소직사각형 {
     public int solution(int[][] sizes) {
         int max = 0;
         int min = 0;
-        for (int[] size : sizes) {
-            int paramMax = Math.max(size[0], size[1]);
-            int paramMin = Math.min(size[0], size[1]);
+        for (int[] e : sizes) {
+            int temp_max = Math.max(e[0], e[1]);
+            int temp_min = Math.min(e[0], e[1]);
 
-            if (paramMax > max) {
-                max = paramMax;
-            }
-
-            if (paramMin > min) {
-                min = paramMin;
-            }
+            if (temp_max > max)
+                max = temp_max;
+            if (temp_min > min)
+                min = temp_min;
         }
-        return min * max;
+        return max * min;
+    }
+
+    public static void main(String[] args) {
+        최소직사각형 answer = new 최소직사각형();
+        System.out.println("answer.solution() = " + answer.solution(new int[][] {{60,50}, {30,70}, {60,30}, {80,40}}));
     }
 }
