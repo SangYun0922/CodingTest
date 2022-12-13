dea9bd5e8c7d06c08765ed6eb82d3c8d3d5ba3de
Sangyun Kim <74143534+SangYun0922@users.noreply.github.com>
2022-12-13 21:08:23 +0900

    Update 롤케이크자르기.java

--- Programmers/level2/롤케이크자르기.java
+++ Programmers/level2/롤케이크자르기.java
@@ -20,12 +20,10 @@ public class 롤케이크자르기 {
         for (int e : topping) {
             map_1.put(e, map_1.getOrDefault(e, 0) + 1);
             
-            if (map_2.containsKey(e)) {
-                if (map_2.get(e) - 1 == 0)
-                    map_2.remove(e);
-                else
-                    map_2.put(e, map_2.get(e) - 1);  
-            }
+            if (map_2.get(e) - 1 == 0)
+                map_2.remove(e);
+            else
+                map_2.put(e, map_2.get(e) - 1);  
             
             if (map_1.size() == map_2.size())
                 answer++;
