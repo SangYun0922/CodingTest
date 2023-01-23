46b8c0615d03df2bef7cfb3d73b172b7cf81030e
Sangyun Kim <74143534+SangYun0922@users.noreply.github.com>
2023-01-23 18:06:30 +0900

    20220123solved

--- baekjoon/그래프/결혼식_5567/Main.java
+++ baekjoon/그래프/결혼식_5567/Main.java
@@ -1,27 +1,23 @@
-package 결혼식_5567;
-
+package baekjoon.결혼식_5567;
 // 친구의 친구까지 초대
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int N;
+    public static int N, answer;
     public static List<List<Integer>> adj;
-    public static Set<Integer> friends;
     public static int[] check;
-    public static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
         adj = new ArrayList<>();
-        friends = new HashSet<>();
-        visited = new boolean[N+1];
         check = new int[N+1];
 
         for (int i = 0; i <= N; i++) {
             adj.add(new ArrayList<>());
+            check[i] = Integer.MAX_VALUE;
         }
 
         for (int i = 0; i < M ; i++) {
@@ -33,34 +29,23 @@ public class Main {
             adj.get(x).add(y);
         }
 
+        dfs(1, 0);
 
-        bfs(1);
-
-        int answer = 0;
         for (int i = 0; i <= N; i++) {
-            System.out.print(check[i] + " ");
             if (check[i] <= 2 && check[i] > 0)
                 answer++;
         }
-        System.out.println();
         System.out.println(answer);
     }
 
-    public static void bfs(int X) {
-        Deque<Integer> deque = new ArrayDeque<>();
-        deque.offer(X);
-        check[X] = 0;
-        while(!deque.isEmpty()) {
-            int cur_r = deque.poll();
+    public static void dfs(int x, int level) {
+        check[x] = Math.min(check[x], level);
+        int next = level + 1;
 
-            for (int i = 0; i < adj.get(cur_r).size(); i++) {
-                int temp = adj.get(cur_r).get(i);
-                if (!visited[temp]) {
-                    check[temp] = check[cur_r] + 1;
-                    friends.add(temp);
-                    deque.offer(temp);
-                    visited[cur_r] = true;
-                }
+        if (level <= 2) {
+            for (int i = 0; i < adj.get(x).size(); i++) {
+                int temp = adj.get(x).get(i);
+                dfs(temp, next);
             }
         }
     }
